8cf285630ab3a107555ca12523b311ffa13b115b
Steven Watremez <steven.watremez@niji.fr>
2017-11-12 17:47:00 +0100

    manage cli with flag

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "flag"
+  "os"
+)
 
 func main() {
-	fmt.Printf("Welcome to xcone !.\n")
+	templatePtr := flag.String("template", "banner", "Template name.")
+  xcVersionPtr := flag.String("xcode-version", "", "Specify an Xcode version to override.")
+  applicationPtr := flag.String("application", "", "Xcode application that you want to change icon.")
+  flag.Parse()
+
+  // test if application is empty
+  if *applicationPtr == "" {
+    flag.PrintDefaults()
+    os.Exit(1)
+  }
+
+  fmt.Printf("Welcome to xcone !.\n")
+
+  fmt.Printf("template: %s, xcode-version: %s, application: %s\n", *templatePtr, *xcVersionPtr, *applicationPtr)
 }
